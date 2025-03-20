6d20bd6a14e6d660fc2b56c9d0c058400be3bf2f
Chao-Ting, Chen <chaotingchen10@gmail.com>
2025-03-20 15:09:32 +0800

    refactor: Migrate to goldmark (#1)
    * refactor: Migrate to goldmark
    
    * refactor: Update template

--- converter/converter.go
+++ converter/converter.go
@@ -1,21 +1,30 @@
 package converter
 
 import (
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
+	"bytes"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
-func Md2HTML(md []byte) ([]byte, error) {
-	// create markdown parser with extensions
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.Mmark
-	p := parser.NewWithExtensions(extensions)
-	doc := p.Parse(md)
+func Md2HTML(source []byte) ([]byte, error) {
+	md := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+		),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+		),
+	)
+	var buf bytes.Buffer
+	if err := md.Convert(source, &buf); err != nil {
+		return nil, err
+	}
 
-	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{Flags: htmlFlags}
-	renderer := html.NewRenderer(opts)
-
-	return markdown.Render(doc, renderer), nil
+	return buf.Bytes(), nil
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.1
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0
-	github.com/gomarkdown/markdown v0.0.0-20240930133441-72d49d9543d8
+	github.com/yuin/goldmark v1.7.8
 )
 
 require golang.org/x/sys v0.4.0 // indirect
